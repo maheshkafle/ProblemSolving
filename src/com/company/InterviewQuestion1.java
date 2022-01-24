53d410ceb351af5b9ebba4b98c1dc270bd353304
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-01-24 18:44:37 +0400

    Initial commit

---
+++ src/com/company/InterviewQuestion1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class InterviewQuestion1 {
+
+    public static void main(String[] args) {
+
+        /*
+        *  Print from 1 to 100 using without using any numbers
+        *
+        */
+
+        int one = 'a'/'a';
+
+        String s = "ssssssssss";
+
+        for (int i = one ;  i<= (s.length() * s.length()); i++){
+            System.out.println(i);
+        }
+    }
+}
