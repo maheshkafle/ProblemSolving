c67da23935af1b2e4f6d148f4885c2cc6e7a7c81
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-01-24 22:56:45 +0400

    Print Text without using any semi colon

---
+++ src/com/company/InterviewQuestion4.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class InterviewQuestion4 {
+
+    public static void main(String[] args) {
+
+        /*
+        * Program to print Hello World without using semi colon(;)
+        *
+        */
+
+        //1.
+        if (System.out.printf("Hello World!" + "\n") == null){
+
+        }
+
+        //2.
+        if (System.out.append("Hello World!" + "\n") == null){
+
+        }
+
+        //3
+        if (System.out.append("Hello World!").equals(null)){
+
+        }
+    }
+}
